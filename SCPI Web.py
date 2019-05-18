d9811a7decbfec94cd481e63e4da2d925889d0bc
jasonjf <jasonf@reutech.co.za>
2019-05-18 12:14:53 +0200

    dropdown list now working

--- SCPI Web.py
+++ SCPI Web.py
@@ -1,6 +1,6 @@
 import eel
 import visa
-
+import random
 rm = visa.ResourceManager()
 instrumentation = rm.list_resources()
 # print(instrumentation)
@@ -8,7 +8,14 @@ instrumentation = rm.list_resources()
 @eel.expose                         # Expose this function to Javascript
 def sendInst():
     return(instrumentation)
+@eel.expose
+def py_random():
+    return random.random()
+
+def print_num(n):
+    print('Got this from Javascript:', n)
+
 
 eel.init('web', allowed_extensions=['.js', '.html'])
 eel.init('static_web_folder')
-eel.start('SCPI.html')
+eel.start('SCPI.html', size=(500,500))
