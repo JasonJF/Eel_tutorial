1dd6279a20a8d9ac5aebeb3418713b2c755952db
jasonjf <jasonf@reutech.co.za>
2019-05-20 16:30:08 +0200

    IDN button has onclick event

--- SCPI Web.py
+++ SCPI Web.py
@@ -3,6 +3,7 @@ import visa
 import random
 rm = visa.ResourceManager()
 instrumentation = rm.list_resources()
+wfg = rm.open_resource('USB0::0x0957::0x1607::MY50003327::INSTR')
 # print(instrumentation)
 
 @eel.expose                         # Expose this function to Javascript
@@ -15,7 +16,14 @@ def py_random():
 def print_num(n):
     print('Got this from Javascript:', n)
 
+@eel.expose
+def idnButton():
+    print("IDN pressed")
+    myInstID = wfg.query('*IDN?')
+    print(myInstID)
+
+
 
 eel.init('web', allowed_extensions=['.js', '.html'])
 eel.init('static_web_folder')
-eel.start('SCPI.html', size=(500,500))
+eel.start('SCPI.html', size=(550,550))
